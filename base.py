eca28ed37f843e362029aee4ab0cb5d7e918e75d
hekaiyou <hekaiyou@qq.com>
2020-12-23 10:46:48 +0800

    2020/12/23 1046

--- base.py
+++ base.py
@@ -1,6 +1,7 @@
 import os
 import json
 import pymongo
+import logging
 
 # RainBond: https://www.rainbond.com/docs/
 # Restful API: https://www.runoob.com/w3cnote/restful-architecture.html
@@ -12,12 +13,12 @@ def create_db_connection(home_kye='MONGODB_HOST', port_kye='MONGODB_PORT'):
     mongo_port = os.environ.get(port_kye, 27017)
 
     if not mongo_home or not mongo_port:
-        print('MongoDB(组件)的组件连接信息是不完整的')
+        logging.error('MongoDB(组件)的组件连接信息是不完整的')
     try:
         mongo_client = pymongo.MongoClient(mongo_home, int(mongo_port))
         return mongo_client
     except Exception as err:
-        print('MongoDB(组件)出现未知错误: {0}'.format(err))
+        logging.error('MongoDB(组件)出现未知错误: {0}'.format(err))
 
 
 def parameter_verification(request, checking=None):
